8e7441b62116e1f2a3507de721caf1999a210b3e
Leonhard Gahr <leonhard.gahr@gmail.com>
2017-11-24 11:27:54 +0100

    add some things

--- src/main/java/backend/podcast/PodcastMP3.java
+++ src/main/java/backend/podcast/PodcastMP3.java
@@ -1,19 +1,50 @@
 package backend.podcast;
 
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+import org.farng.mp3.id3.AbstractID3v2;
+
+import java.io.File;
+import java.io.IOException;
+
 public class PodcastMP3 {
-    private String path;
+    private MP3File mp3File;
+    private AbstractID3v2 id3Tags;
+
     private String ID3_Title;
     private String ID3_Artist;
+    private String ID3_Ablum;
     private String ID3_ReleaseYear;
     private String ID3_Comment;
     private String ID3_Genre;
 
-    public String getPath() {
-        return path;
+    private int bitrate;
+
+    public PodcastMP3(String path) throws IOException, TagException {
+        this.mp3File = new MP3File(new File(path));
+
+        // parse information from mp3 header
+        mp3File.seekMP3Frame();
+
+        this.id3Tags = mp3File.getID3v2Tag();
+
+        this.ID3_Title = this.id3Tags.getSongTitle();
+        this.ID3_Artist = this.id3Tags.getLeadArtist();
+        this.ID3_ReleaseYear = this.id3Tags.getYearReleased();
+        this.ID3_Comment = this.id3Tags.getSongComment();
+        this.ID3_Genre = this.id3Tags.getSongComment();
+
+        this.bitrate = mp3File.getBitRate();
     }
 
-    public void setPath(String path) {
-        this.path = path;
+    private void updateTags() {
+        try {
+            mp3File.save();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TagException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getID3_Title() {
@@ -21,6 +52,8 @@ public class PodcastMP3 {
     }
 
     public void setID3_Title(String ID3_Title) {
+        id3Tags.setSongTitle(ID3_Title);
+        this.updateTags();
         this.ID3_Title = ID3_Title;
     }
 
@@ -29,6 +62,8 @@ public class PodcastMP3 {
     }
 
     public void setID3_Artist(String ID3_Artist) {
+        this.id3Tags.setLeadArtist(ID3_Artist);
+        this.updateTags();
         this.ID3_Artist = ID3_Artist;
     }
 
@@ -37,6 +72,8 @@ public class PodcastMP3 {
     }
 
     public void setID3_ReleaseYear(String ID3_ReleaseYear) {
+        this.id3Tags.setYearReleased(ID3_ReleaseYear);
+        this.updateTags();
         this.ID3_ReleaseYear = ID3_ReleaseYear;
     }
 
@@ -45,6 +82,8 @@ public class PodcastMP3 {
     }
 
     public void setID3_Comment(String ID3_Comment) {
+        this.id3Tags.setSongComment(ID3_Comment);
+        this.updateTags();
         this.ID3_Comment = ID3_Comment;
     }
 
@@ -53,6 +92,26 @@ public class PodcastMP3 {
     }
 
     public void setID3_Genre(String ID3_Genre) {
+        this.id3Tags.setSongGenre(ID3_Genre);
+        this.updateTags();
         this.ID3_Genre = ID3_Genre;
+    }
+
+    public int getBitrate() {
+        return bitrate;
+    }
+
+    public void setBitrate(int bitrate) {
+        this.bitrate = bitrate;
+    }
+
+    public String getID3_Ablum() {
+        return ID3_Ablum;
+    }
+
+    public void setID3_Ablum(String ID3_Ablum) {
+        this.id3Tags.setAlbumTitle(ID3_Ablum);
+        this.updateTags();
+        this.ID3_Ablum = ID3_Ablum;
     }
 }

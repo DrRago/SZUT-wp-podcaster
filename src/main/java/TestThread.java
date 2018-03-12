baec4dfe0fccb01a493fb42b34c258b695afc27c
Leonhard Gahr <leonhard.gahr@gmail.com>
2018-03-12 10:32:01 +0100

    add Threading demo =)

---
+++ src/main/java/TestThread.java
@@ -0,0 +1,62 @@
+import backend.fileTransfer.Protocols;
+import backend.fileTransfer.Uploader;
+import backend.fileTransfer.UploaderException;
+import backend.fileTransfer.UploaderFactory;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+class RunnableDemo implements Runnable {
+    private Thread t;
+    private String threadName;
+    private Uploader uploader;
+
+    RunnableDemo( String name) {
+        threadName = name;
+        System.out.println("Creating " +  threadName );
+    }
+
+    public void run() {
+        System.out.println("Running " +  threadName );
+        try {
+            uploader = UploaderFactory.getUploader(Protocols.FTPS, "tandashi.de", 990, "root", "12345", "/uploads/");
+        } catch (UploaderException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+
+    public void start () {
+        System.out.println("Starting " +  threadName );
+        if (t == null) {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+}
+
+public class TestThread extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> {
+            RunnableDemo R1 = new RunnableDemo( "Thread-1");
+            R1.start();
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
